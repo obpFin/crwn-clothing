0523dcaaa3c396aa98c6904fee05bae60fae256f
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-01-18 09:43:52 +0200

    added signin page

---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import SignIn from '../../components/signin/signin.component'
+
+import './signin-and-signup.styles.scss'
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+export  default SignInAndSignUpPage
