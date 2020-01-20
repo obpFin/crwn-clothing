c647fb724565d704c3f00d10c89a63b9115e3258
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-01-20 16:37:21 +0200

    google signin styles

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -51,8 +51,12 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             required
           />
-          <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignin>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
