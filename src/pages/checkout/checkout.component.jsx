f4142b3864236134d472ae5a403961e192232879
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-02-10 19:09:37 +0200

    integrated stripe checkout

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -38,6 +39,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className='total'>
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use following test card for payments*
+        <br/>
+        4242 4242 4242 4242 - exp: 04/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
