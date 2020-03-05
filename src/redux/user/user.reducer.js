51d7f717d0c7fa29b446cff6c31031caf6254885
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-03-05 21:20:06 +0200

    redux flow for logout

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -13,11 +13,18 @@ const userReducer = (state = INITIAL_STATE, action) => {
         currentUser: action.payload,
         error: null
       };
+    case userActionTypes.SIGN_OUT_SUCCESS:
+      return {
+        ...state,
+        currentUser: null,
+        error: null
+      };
     case userActionTypes.SIGN_IN_FAILURE:
+    case userActionTypes.SIGN_OUT_FAILURE:
       return {
         ...state,
         error: action.payload
-      }
+      };
     default:
       return state;
   }
