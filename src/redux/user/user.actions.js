53992064e7ff42298a7b16c63a81aa1c0342ead2
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-03-08 18:57:31 +0200

    signUpStart actions

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -35,3 +35,17 @@ export const signOutFailure = error => ({
   type: UserActionTypes.SIGN_OUT_FAILURE,
   payload: error
 });
+
+export const signUpStart = () => ({
+  type: UserActionTypes.SIGN_UP_START
+});
+
+export const signUpSuccess = user => ({
+  type: UserActionTypes.SIGN_UP_SUCCESS,
+  payload: user
+});
+
+export const signUpFailure = error => ({
+  type: UserActionTypes.SIGN_UP_FAILURE,
+  payload: error
+});
