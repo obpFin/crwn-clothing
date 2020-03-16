3ba3685f0715acc0931311f2556e6492d0d2c1a2
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-03-16 16:49:52 +0200

    send request to backend route with payment info

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_yNNtayV2fQz6vlGQSYRp46qH00K3Nm8hJO';
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment Succesfull!');
+      })
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert('There was an issue with your payment. Please make sure you use the provided credit card.')
+      });
   };
   return (
     <StripeCheckout
@@ -24,4 +38,4 @@ const StripeCheckoutButton = ({ price }) => {
   );
 };
 
-export default StripeCheckoutButton
+export default StripeCheckoutButton;
