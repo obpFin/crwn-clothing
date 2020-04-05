d0c17ec258215766e6ee4ab0ef276e567c228e82
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-04-05 18:31:21 +0300

    use static & https enforcing only in prod

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(compression());
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
   app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
