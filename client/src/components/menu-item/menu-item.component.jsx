669ad01a3186f3d068b70b026c02ab4ff7268e06
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-04-12 12:02:11 +0300

    menu-item tests

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -2,30 +2,32 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 import {
+  MenuItemContainer,
   BackgroundImageContainer,
   ContentContainer,
-  ContentSubtitle,
   ContentTitle,
-  MenuItemContainer
+  ContentSubtitle
 } from './menu-item.styles';
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+export const MenuItem = ({
+  title,
+  imageUrl,
+  size,
+  history,
+  linkUrl,
+  match
+}) => (
   <MenuItemContainer
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}
+    size={size}
     onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <BackgroundImageContainer
       className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    ></BackgroundImageContainer>
+      imageUrl={imageUrl}
+    />
     <ContentContainer className='content'>
-      <ContentTitle className='title'>{title.toUpperCase()}</ContentTitle>
-      <ContentSubtitle className='subtitle'>SHOP NOW</ContentSubtitle>
+      <ContentTitle>{title.toUpperCase()}</ContentTitle>
+      <ContentSubtitle>SHOP NOW</ContentSubtitle>
     </ContentContainer>
   </MenuItemContainer>
 );
